93922d7d2e3faa38a34a25b7974bcd7b9bdc5764
kxxoling <kxxoling@gmail.com>
2017-10-04 22:01:57 +0800

    script: pixiv_search_overlay_remover.user.js: Remove Pixiv search page overlay.

---
+++ pixiv_search_overlay_remover.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Pixiv Search Overlay Remover
+// @namespace    https://windrunner.me/
+// @version      0.1
+// @description  Remove Pixiv search page overlay
+// @author       Kane Blueriver
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
+// @match        https://www.pixiv.net/search.php*
+// ==/UserScript==
+
+/* jshint ignore:start */
+var inline_src = (<><![CDATA[
+/* jshint ignore:end */
+    /* jshint esnext: false */
+    /* jshint esversion: 6 */
+
+    document.querySelector('.popular-introduction-overlay').remove();
+
+/* jshint ignore:start */
+]]></>).toString();
+var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
+eval(c.code);
+/* jshint ignore:end */
