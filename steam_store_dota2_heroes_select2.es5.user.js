ec1813c873f0ff857bd49b3708f70e12197056ef
kxxoling <kxxoling@gmail.com>
2017-12-28 23:54:18 +0800

    Using @resource to import CSS file.

--- steam_store_dota2_heroes_select2.es5.user.js
+++ steam_store_dota2_heroes_select2.es5.user.js
@@ -1,21 +1,20 @@
 // ==UserScript==
 // @name         Dota2 Heroes Select2 ES5
 // @namespace    https://windrunner.me/
-// @version      0.1.0
+// @version      0.1.1
 // @description  A jQuery Select2 extended Dota2 heroes select menu
 // @author       Kane Blueriver
 // @match        https://steamcommunity.com/market/search*appid=570*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/js/select2.min.js
-// @grant        none
+// @resource     select2Css https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css
+// @grant        GM_getResourceText
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var link = document.createElement( "link" );
-    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.6-rc.0/css/select2.min.css';
-    link.type = "text/css";
-    link.rel = "stylesheet";
-    document.head.appendChild(link);
+    GM_addStyle(GM_getResourceText('select2Css'));
+
     (function($) {
         var advancedOptions = document.querySelector('.market_search_advanced_button');
         advancedOptions.addEventListener('click', function() {
